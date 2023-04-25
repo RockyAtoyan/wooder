3ed7a922e126a408da56040036bee8cb17a8c90a
Rocky <atoyanrobert21@gmail.com>
2023-04-25 20:42:03 +0300

    Fix menu bug

--- js/script.js
+++ js/script.js
@@ -40,16 +40,16 @@ const pageSlider = new Swiper('.wrapper', {
     mousewheel: {
         sensitivity: 1
     },
-    slidesPerView:true,
+    slidesPerView: true,
     watchOverflow: true,
     // allowTouchMove: false,
     // freeMode: true,
-    breakpoints:{
-        320:{
-            autoHeight:true,
+    breakpoints: {
+        320: {
+            autoHeight: true,
         },
-        600:{
-            autoHeight:false,
+        600: {
+            autoHeight: false,
         },
 
     },
@@ -59,7 +59,7 @@ const pageSlider = new Swiper('.wrapper', {
         clickable: true,
         bulletClass: 'bullet',
         bulletActiveClass: 'active',
-        renderBullet:(index,className) => `<button class="${className}" ${index === 2 || index === 4 ? 'style="display:none"' : ''}></button>`,
+        renderBullet: (index, className) => `<button class="${className}" ${index === 2 || index === 4 ? 'style="display:none"' : ''}></button>`,
     },
     init: false,
     on: {
@@ -80,10 +80,10 @@ const pageSlider = new Swiper('.wrapper', {
     observer: true,
     observeParents: true,
     observeSlideChildren: true,
-    scrollbar:{
-        el:'.page_scroll',
+    scrollbar: {
+        el: '.page_scroll',
         dragClass: 'page_scroll__drag',
-        draggable:true
+        draggable: true
     }
 })
 
@@ -99,6 +99,7 @@ for (let i = 0; i < menuLinks.length; i++) {
         burger.classList.remove('active')
     })
 }
+
 /*---------------------*/
 
 function menuActive() {
@@ -128,97 +129,95 @@ headerLogo.addEventListener('click', () => {
     pageSlider.slideTo(0)
 })
 
-burger.addEventListener('click',(event) => {
-    console.log(event.target)
-    if(event.target.parentNode.classList.contains('burger') ||event.target.classList.contains('burger') ){
-         generalMenu.classList.toggle('active')
-         burger.classList.toggle('active')
-    }else {
-        if(!event.target.parentNode.classList.contains('general_menu') && !event.target.parentNode.classList.contains('burger_menu')) {
+document.addEventListener('click', (event) => {
+        if (event.target.closest('.burger')) {
+            generalMenu.classList.toggle('active')
+            burger.classList.toggle('active')
+        }
+        if (!event.target.closest('.burger_menu')) {
             generalMenu.classList.remove('active')
             burger.classList.remove('active')
         }
     }
-})
+)
 
 
 
+    function setScrollType() {
+        if (wrapper.classList.contains('_free')) {
+            wrapper.classList.remove('_free');
+            pageSlider.params.freeMode.enabled = false;
+        }
+        // if(window.innerWidth <= 768){
+        //     pageSlider.params.freeMode.enabled = true;
+        // }
+        for (let index = 0; index < pageSlider.slides.length; index++) {
+            const pageSlide = pageSlider.slides[index];
+            const pageSlideContent = pageSlide.querySelector('section');
+            if (pageSlideContent) {
+                const pageSlideContentHeight = pageSlideContent.offsetHeight;
+                if (pageSlideContentHeight > window.innerHeight) {
 
-function setScrollType() {
-    if (wrapper.classList.contains('_free')) {
-        wrapper.classList.remove('_free');
-        pageSlider.params.freeMode.enabled = false;
-    }
-    // if(window.innerWidth <= 768){
-    //     pageSlider.params.freeMode.enabled = true;
-    // }
-    for (let index = 0; index < pageSlider.slides.length; index++) {
-        const pageSlide = pageSlider.slides[index];
-        const pageSlideContent = pageSlide.querySelector('section');
-        if (pageSlideContent) {
-            const pageSlideContentHeight = pageSlideContent.offsetHeight;
-            if (pageSlideContentHeight > window.innerHeight) {
-
-                wrapper.classList.add('_free');
-                pageSlider.params.freeMode.enabled = true;
-                break;
+                    wrapper.classList.add('_free');
+                    pageSlider.params.freeMode.enabled = true;
+                    break;
+                }
             }
         }
     }
-}
 
 
-pageSlider.init()
+    pageSlider.init()
 
 
-const videosSlider = new Swiper('.videos_slider',{
-    wrapperClass: 'videos_items',
-    slideClass: 'videos_item',
-    init:false,
-    loop:true,
-    touch: true,
-    allowTouchMove:true,
-    breakpoints: {
-        990:{
-            enabled:false,
-            slidesPerView: 3
+    const videosSlider = new Swiper('.videos_slider', {
+        wrapperClass: 'videos_items',
+        slideClass: 'videos_item',
+        init: false,
+        loop: true,
+        touch: true,
+        allowTouchMove: true,
+        breakpoints: {
+            990: {
+                enabled: false,
+                slidesPerView: 3
+            }
         }
-    }
-})
-
-videosSlider.init()
-
-/* -------------------------------------------- */
-
-const src = "https://www.youtube.com/embed/vkab3w9zeI8"
-
-const langMode = document.querySelector('.header_lang h3')
-const langItems = document.querySelectorAll('.header_lang__items span')
-const popupVideos = document.querySelectorAll('.popup_video iframe')
-
-langItems.forEach((node) => {
-    node.addEventListener('click',() => {
-        langItems.forEach(el => el.classList.remove('active'))
-        node.classList.add('active')
-        langMode.innerHTML = node.innerHTML
     })
-})
 
-for (let i = 0; i < videoBtns.length; i++) {
-    const btn = videoBtns[i]
-    const popup = popups[i]
-    btn.addEventListener('click',() => {
-        popup.classList.add('active')
-        popupVideos.forEach(el => {
-            el.setAttribute('src',src)
+    videosSlider.init()
+
+    /* -------------------------------------------- */
+
+    const src = "https://www.youtube.com/embed/vkab3w9zeI8"
+
+    const langMode = document.querySelector('.header_lang h3')
+    const langItems = document.querySelectorAll('.header_lang__items span')
+    const popupVideos = document.querySelectorAll('.popup_video iframe')
+
+    langItems.forEach((node) => {
+        node.addEventListener('click', () => {
+            langItems.forEach(el => el.classList.remove('active'))
+            node.classList.add('active')
+            langMode.innerHTML = node.innerHTML
         })
     })
-    popup.addEventListener('click',() => {
-        if(!event.target.classList.contains('popup') && (!event.target.classList.contains('popup_title') && !event.target.classList.contains('popup_content') && !event.target.parentNode.classList.contains('popup_content'))){
-            popup.classList.remove('active')
+
+    for (let i = 0; i < videoBtns.length; i++) {
+        const btn = videoBtns[i]
+        const popup = popups[i]
+        btn.addEventListener('click', () => {
+            popup.classList.add('active')
             popupVideos.forEach(el => {
-                el.setAttribute('src','')
+                el.setAttribute('src', src)
             })
-        }
-    })
-}
+        })
+        popup.addEventListener('click', () => {
+            if (!event.target.classList.contains('popup') && (!event.target.classList.contains('popup_title') && !event.target.classList.contains('popup_content') && !event.target.parentNode.classList.contains('popup_content'))) {
+                popup.classList.remove('active')
+                popupVideos.forEach(el => {
+                    el.setAttribute('src', '')
+                })
+            }
+        })
+    }
